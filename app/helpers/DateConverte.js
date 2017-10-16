866a42a33ba3fd222ded74a2e8ad8387a7c0a79a
José Ferreira da Silva <ferreirajosejp@gmail.com>
2017-10-16 18:36:01 -0200

    implementação dos metodos para converte string para data e data para string com algumas validações

--- app/helpers/DateConverte.js
+++ app/helpers/DateConverte.js
@@ -3,15 +3,24 @@ export class DateConverte {
     constructor(){}
 
     stringToDate(string){
+        if(typeof string == string){
 
-       let date = string.split('/');
-       let newDate = new Date(date[2], date[1], date[0]);
+            let date = string.split('/');
+            let newDate = new Date(date[2], date[1], date[0]);
+            return newDate;
 
-       return newDate;
+        }else{
+            console.log('Formato de data Invalido. Informe 02/20/1991');
+        }
     }
 
-    
+    //data.getDate() + " / " + data.getMonth() + 1 + " / "+  data.getFullYer();
     dateToString(data){
-        console.log(data);
+        if(typeof data == Date){
+            let string = `${data.getDate() + " / " + data.getMonth() + 1 + " / "+  data.getFullYer()}`;
+            return string;
+        }else{
+            console.log('Não é um data valida');
+        }        
     }
 }
